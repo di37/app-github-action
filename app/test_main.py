dbaf3e8992d65635828882693f7a79b1db2e971f
Isham Rashik <d.isham.993@gmail.com>
2024-09-06 22:54:12 +0000

    Initial commit.

---
+++ app/test_main.py
@@ -0,0 +1,15 @@
+# app/test_main.py
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, FastAPI!"}
+
+def test_read_item():
+    response = client.get("/items/1?q=foo")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1, "q": "foo"}
